38b74d295189577d4a4bd067a99a344a7f7315a7
Elliot Dickison <ejdickison@gmail.com>
2015-09-07 17:52:35 -0700

    Add a linting task to gulp and jshint all javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jsxhint = require('jshint-jsx').JSXHINT;
 var browserify = require('gulp-browserify');
 var uglify = require('gulp-uglify');
 
+gulp.task('lint', function(){
+    gulp.src('src/**/*.js')
+        .pipe(jshint({
+            linter: jsxhint,
+            esnext: true,
+        }))
+        .pipe(jshint.reporter('default'));
+});
+
 gulp.task('browserify', function(){
     gulp.src('src/client.js')
-        .pipe(browserify({transform: 'babelify'}))
+        .pipe(browserify({
+            transform: 'babelify',
+        }))
         .pipe(uglify())
         .pipe(gulp.dest('public/js'));
 });
