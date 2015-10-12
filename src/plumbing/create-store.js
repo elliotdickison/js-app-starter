87718fdcd0e714979158bf0e92caedc2c558d213
Elliot Dickison <ejdickison@gmail.com>
2015-10-11 21:54:39 -0700

    Cleanup the create-store module

--- src/plumbing/create-store.js
+++ src/plumbing/create-store.js
@@ -1,8 +1,8 @@
 /**
- * Creates a redux store with the correct middleware for the current environment
- * (e.g. dev tooling middleware is included in development only). Implements a
- * custom reducer combination technique that allows the application state to be
- * an ImmutableJS object instead of a plain javascript object.
+ * Creates a redux store with the correct enhancers for the current environment
+ * (e.g. dev tools when building for development). Implements custom reducer
+ * combining that allows the application state to be an ImmutableJS object
+ * instead of the plain javascript object that Redux expects by default.
  *
  * @module
  */
@@ -42,7 +42,7 @@ function applyReducers (reducers, state = emptyState, action = emptyAction) {
  * @returns {Function} A mega-reducer that can be used to easily run all of the
  * individual reducers
  */
-function combineImmutableReducers (reducers) {
+function combineReducers (reducers) {
   var defaultState = applyReducers(reducers);
   return function combination (state = defaultState, action = emptyAction) {
     return applyReducers(reducers, state, action);
@@ -50,18 +50,19 @@ function combineImmutableReducers (reducers) {
 }
 
 /**
- * Builds and returns an array of middleware to be applied to the Redux store
+ * Builds and returns an array of store enhancers to be applied to the main
+ * Redux store
  *
- * @returns {Function[]} An array of middleware
+ * @returns {Function[]} An array of Redux store enhancers
  */
-function getMiddleware () {
-  let middleware = [applyMiddleware(thunk)];
+function getStoreEnhancers () {
+  let storeEnhancers = [applyMiddleware(thunk)];
   if (__DEVELOPMENT__ && __CLIENT__) {
     const { devTools, persistState } = require('redux-devtools');
-    middleware.push(devTools());
-    middleware.push(persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/)));
+    storeEnhancers.push(devTools());
+    storeEnhancers.push(persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/)));
   }
-  return middleware;
+  return storeEnhancers;
 }
 
 /**
@@ -72,9 +73,9 @@ function getMiddleware () {
  * @returns {Object} A Redux store
  */
 export default function createStore (initialState) {
-  const rootReducer = combineImmutableReducers({
+  const rootReducer = combineReducers({
     widgets
   });
-  const createStoreWithMiddleware = compose.apply(null, getMiddleware())(createReduxStore);
-  return createStoreWithMiddleware(rootReducer, initialState);
+  const createStoreWithEnhancers = compose.apply(null, getStoreEnhancers())(createReduxStore);
+  return createStoreWithEnhancers(rootReducer, initialState);
 }
